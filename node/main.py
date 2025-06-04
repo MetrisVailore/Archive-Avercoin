41e05c94809f073650742f60a6ca9599888ecb0b
AEhehe <aeahb003805mg@ahbs.ae>
2025-06-04 13:19:10 +0400

    Update main.py

--- node/main.py
+++ node/main.py
@@ -15,7 +15,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-import sys
+import sys, os
 
 # Add the path to the parent directory of 'blockchain'
 sys.path.append(os.path.abspath('../blockchain'))
