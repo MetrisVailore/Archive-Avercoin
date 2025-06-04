2feac439ad22a781d5d8b766ccb50d9d83f169ba
AEhehe <aeahb003805mg@ahbs.ae>
2025-06-04 12:48:31 +0400

    Update run.py

--- node/run.py
+++ node/run.py
@@ -1,4 +1,9 @@
 import uvicorn
+import ctypes
+
+kernel32 = ctypes.windll.kernel32
+
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=3006, reload=True)
